729b0d54b267e16fbcfa9a81dbe7f19af9208dc1
AniketS-cpu <54439994+AniketS-cpu@users.noreply.github.com>
2023-01-24 14:15:17 +0530

    Update oauth.cs

--- Chapter-7/oauth.cs
+++ Chapter-7/oauth.cs
@@ -2,7 +2,7 @@
     string url = "https://contoso.crm.dynamics.com";
     string clientId = "51f81489-12ee-4a9e-aaae-a2591f45987d";
     string username = "user@contoso.onmicrosoft.com";
-    string password = "Pass@rowd1";
+    string password = "Pass@word1";
     AuthenticationContext authContext = new AuthenticationContext("https://login.microsoftonline.com/common", false);
     UserCredential credential = new UserCredential(username, password);
     AuthenticationResult result = authContext.AcquireToken(url, clientId, credential);
